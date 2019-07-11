d674c78984d57eb4e614542f35b128d69744e03b
thebaron24 <thebaron24@gmail.com>
2019-07-11 19:28:55 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,9 @@ export class AppComponent  {
   constructor(
     iconReg: MatIconRegistry, sanitizer: DomSanitizer
     ) {
-      // iconReg.addSvgIconSet(sanitizer.bypassSecurityTrustResourceUrl('/node_modules/material-design-icons/iconfont/MaterialIcons-Regular.svg#MaterialIcons-Regular'));
+      iconReg.addSvgIconSet(sanitizer.bypassSecurityTrustResourceUrl('/node_modules/material-design-icons/iconfont/MaterialIcons-Regular.svg#MaterialIcons-Regular'));
+
+      console.log(iconReg.getDefaultFontSetClass());
   }
 
   toggleAnimate(){
